ea56341b8b9cdfe3f211258bbf5215fa332f255e
Bakytai Egemberdieva <bakijw@gmail.com>
2022-03-11 22:07:47 +0600

    Вывела детали поста, все коментарии к данному посту, подписываюсь на юзера, что бы в последующем для создания комментарий брать токен

--- frontend/src/app/pages/post/post.component.ts
+++ frontend/src/app/pages/post/post.component.ts
@@ -1,15 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Comment } from '../../models/comment.model';
+import { Observable, Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { ActivatedRoute } from '@angular/router';
+import { AppState } from '../../store/types';
+import { Post } from '../../models/post.model';
+import { fetchPostRequest } from '../../store/posts.actions';
+import { fetchCommentsRequest } from '../../store/comments.actions';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.sass']
 })
-export class PostComponent implements OnInit {
+export class PostComponent implements OnInit , OnDestroy{
+  comments: Observable<Comment[]>;
+  post: Observable<Post | null>;
+  postInfo!: Post;
+  postSubscription!: Subscription;
+  loading: Observable<boolean>;
+  error: Observable<null | string>;
+  user: Observable<null | User>;
+  userSub!: Subscription;
+  postId!: string;
+  token!: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private store: Store<AppState>, private route: ActivatedRoute) {
+    this.post = store.select(state => state.posts.post);
+    this.comments = store.select(state => state.comments.comments);
+    this.loading = store.select(state => state.comments.fetchLoading);
+    this.error = store.select(state => state.comments.fetchError);
+    this.user = store.select(state => state.users.user);
   }
 
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.postId = params['id']
+    })
+    this.store.dispatch(fetchPostRequest({id: this.postId}));
+    this.store.dispatch(fetchCommentsRequest({id: this.postId}));
+
+    this.postSubscription = this.post.subscribe(post => {
+      if (post) {
+        this.postInfo = post;
+      }
+    });
+
+    this.userSub = this.user.subscribe(user => {
+      if (user) {
+        this.token = user.token;
+      } else {
+        this.token = '';
+      }
+    })
+  }
+
+  ngOnDestroy() {
+    this.postSubscription.unsubscribe();
+    this.userSub.unsubscribe();
+  }
 }
